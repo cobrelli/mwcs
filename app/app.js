9dfe2d1fb4e823246a8391921e8bb0aec37df71e
Tomi Virtanen <cobrelli@gmail.com>
2016-06-11 10:28:41 +0300

    Implement filereader directive

--- app/app.js
+++ app/app.js
@@ -6,16 +6,39 @@ require('lodash');
 angular.module('mwcs', ['ui.router', 'ngResource']);
 
 function mwcsCtrl($scope) {
+    $scope.encoding = 'ISO-8859-1';
+    $scope.salaries = '';
+
+    $scope.$watch('salaries', function (salaries) {
+        console.log('file changed', salaries);
+    });
 }
 angular.module('mwcs').controller('mwcsCtrl', ['$scope', mwcsCtrl]);
 
 angular.module('mwcs').directive('fileReader', [function () {
     return {
         scope: {
-            encoding: '='
+            encoding: '=',
+            results: '=salaries'
         },
         template: '<input type="file"></input>',
         link: function (scope, element) {
+            element.on('change', function (event) {
+                console.log('element changed');
+                var fileReader = new FileReader();
+                fileReader.onload = function (onLoadEvent) {
+                    scope.$apply(function () {
+                        var content = onLoadEvent.target.result.replace(/\r\n|\r/g, '\n');
+                        var lines = content.split('\n');
+                        var header = lines.splice(0, 1);
+                        scope.results = {
+                            header: header[0].split(','),
+                            content: lines
+                        }
+                    });
+                };
+                fileReader.readAsText((event.srcElement || event.target).files[0], scope.encoding);
+            });
         }
     }
 }]);
