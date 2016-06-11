f5d6457b939aba88b782179fba1a35915a7300eb
Tomi Virtanen <cobrelli@gmail.com>
2016-06-11 12:11:06 +0300

    Implement SalaryService

--- app/app.js
+++ app/app.js
@@ -15,6 +15,54 @@ function mwcsCtrl($scope) {
 }
 angular.module('mwcs').controller('mwcsCtrl', ['$scope', mwcsCtrl]);
 
+function SalaryService() {
+    var wage = 3.75;
+    var eveningWorkCompensation = 1.15;
+    var overtimeFirstTwoHours = wage * 1.25;
+    var overtimeNextTwoHour = wage * 1.5;
+    var overtimeFurther = wage * 2;
+
+    function calculateTotalDailyPay(dailyWage, EveningCompensation, overtimeCompensation) {
+        return dailyWage + eveningWorkCompensation + overtimeCompensation;
+    }
+
+    function calculateDailyWage(hours) {
+        return hours * wage;
+    }
+
+    function calculateEveningWorkCompensation(hours) {
+        return hours * (wage + eveningWorkCompensation);
+    }
+
+    function calculateOvertimeCompensation(hours) {
+        var compensation = 0;
+        if (hours > 2) {
+            compensation += 2 * overtimeFirstTwoHours * wage;
+            hours -= 2;
+        } else {
+            compensation += hours * overtimeFirstTwoHours * wage;
+            return compensation
+        }
+        if (hours > 2) {
+            compensation += 2 * overtimeNextTwoHour * wage;
+            hours -= 2;
+        } else {
+            compensation += hours * overtimeNextTwoHour * wage;
+            return compensation
+        }
+        compensation += hours * overtimeFurther * wage;
+        return compensation;
+    }
+    
+    return {
+        calculateTotalDailyPay: calculateTotalDailyPay,
+        calculateDailyWage: calculateDailyWage,
+        calculateEveningWorkCompensation: calculateEveningWorkCompensation,
+        calculateOvertimeCompensation: calculateOvertimeCompensation
+    };
+}
+angular.module('mwcs').service('SalaryService', SalaryService);
+
 angular.module('mwcs').directive('fileReader', [function () {
     return {
         scope: {
