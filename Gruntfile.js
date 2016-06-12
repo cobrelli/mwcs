727fd9b5d16fd59c9b44c1432412c5c9dfca62d3
Tomi Virtanen <cobrelli@gmail.com>
2016-06-12 09:20:06 +0300

    Add grunt task for running mocha tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   grunt.initConfig({
     jshint: {
       files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
@@ -12,28 +12,35 @@ module.exports = function(grunt) {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint']
     },
+    mochaTest: {
+      test: {
+        src: ['tests/**/*.js'],
+      },
+    },
     webpack: {
-        build: {
-            entry: './app/app.js',
-            output: {
-                path: 'public',
-                filename: 'build.js'
-            }
+      build: {
+        entry: './app/app.js',
+        output: {
+          path: 'public',
+          filename: 'build.js'
         }
+      }
     },
     execute: {
-		target: {
-			src: ['server.js']
-		}
-	}
+      target: {
+        src: ['server.js']
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-webpack');
   grunt.loadNpmTasks('grunt-execute');
+  grunt.loadNpmTasks('grunt-mocha-test');
 
   grunt.registerTask('default', ['jshint']);
   grunt.registerTask('build', ['webpack:build']);
   grunt.registerTask('serve', ['webpack:build', 'execute']);
+  grunt.registerTask('test', ['mochaTest']);
 };
